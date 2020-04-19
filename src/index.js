e3e0d3d6eff116fbfb437737bb8bb3adebaa6115
Federico Martin <fmartin@MacBook-Pro-de-Federico.local>
2020-04-19 04:26:28 -0300

    changes to index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import SearchGrammar from './interpreter/grammar';
-import { join } from 'path';
-import { mkdirSync, existsSync, writeFileSync, readFileSync } from 'fs';
-import { MODES } from 'syntax-cli/dist/grammar/grammar-mode';
-import Grammar from 'syntax-cli/dist/grammar/grammar';
-import LRParserGenerator from 'syntax-cli/dist/lr/lr-parser-generator-default';
-import header from './header';
 import 'colors';
 
-import 'syntax-cli/dist/bin/syntax';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 
-(() => {
-  build();
-})();
+import Grammar from 'syntax-cli/dist/grammar/grammar';
+import LRParserGenerator from 'syntax-cli/dist/lr/lr-parser-generator-default';
+import { MODES } from 'syntax-cli/dist/grammar/grammar-mode';
+import SearchGrammar from './interpreter/grammar';
+import header from './header';
+import { join } from 'path';
 
 /**
  * Production build.
@@ -20,37 +16,30 @@ import 'syntax-cli/dist/bin/syntax';
  * @see https://github.com/DmitrySoshnikov/syntax
  *
  */
-function build() {
-  var grammarData = Grammar.dataFromString(
-    JSON.stringify(SearchGrammar),
-    'bnf'
-  );
+var grammarData = Grammar.dataFromString(JSON.stringify(SearchGrammar), 'bnf');
 
-  console.log(`✓ loaded grammar file...`.green);
-  var grammar = Grammar.fromData(grammarData, { mode: MODES.SLR1 });
+console.log(`✓ loaded grammar file...`.green);
+var grammar = Grammar.fromData(grammarData, { mode: MODES.SLR1 });
 
-  const grammarTempFile = join(__dirname, 'grammar.temp.js');
-  const headerFile = join(__dirname, './header.txt');
-  const outputDir = join(__dirname, '../dist/');
-  const outputFile = join(outputDir, 'search-operators.js');
-  if (!existsSync(outputDir)) {
-    mkdirSync(outputDir);
-  }
+const grammarTempFile = join(__dirname, 'grammar.temp.js');
+const outputDir = join(__dirname, '../dist/');
+const outputFile = join(outputDir, 'search-operators.js');
 
-  let code = new LRParserGenerator({
-    grammar: grammar,
-    /*     options: {
-      'resolve-conflicts': false,
-    }, */
-  }).generate();
-
-  //Add custom header
-  code = `${header}\n${code}`;
-
-  //Save file and finish
-  writeFileSync(outputFile, code);
-  console.log(`✓ search-operators parser successfully generated`.green);
-
-  //IF
-  writeFileSync(grammarTempFile, JSON.stringify(SearchGrammar));
+//Prepare /lib
+if (!existsSync(outputDir)) {
+  mkdirSync(outputDir);
 }
+
+let code = new LRParserGenerator({
+  grammar: grammar,
+}).generate();
+
+//Add custom header
+code = `${header}\n${code}`;
+
+//Save file and finish
+writeFileSync(outputFile, code);
+console.log(`✓ search-operators parser successfully generated`.green);
+
+//copy generated grammad (validate-grammar script)
+writeFileSync(grammarTempFile, JSON.stringify(SearchGrammar));
