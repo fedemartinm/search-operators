2bd75bf3a7cec128db458a7b04abb4336cbc4db1
Federico Martin <fmartin@MacBook-Pro-de-Federico.local>
2020-04-23 03:31:04 -0300

    Added definition types.

---
+++ src/index.d.ts
@@ -0,0 +1,69 @@
+/** Search Operators declaration file */
+
+/**
+ * Search operator applyed to text.
+ */
+export interface SearchOperator {
+  type: 'match' | 'not-match' | 'exact' | 'exclude';
+  value: string;
+  in?: string;
+}
+
+/**
+ * Parse result object.
+ */
+export interface ParseResult {
+  filters: SearchOperator[];
+  terms: [];
+}
+
+/**
+ * Parse options.
+ * keys:Allowed match operator's keys.
+ */
+export interface ParseOptions {
+  keys: string[];
+}
+
+/**
+ * Smallest element recognized by parser.
+ */
+export interface Token {
+  // Basic data.
+  type:
+    | 'TEXT'
+    | 'INCLUDE_WORD'
+    | 'EXCLUDE_WORD'
+    | 'EXACT_PHRASE'
+    | 'MATCH'
+    | 'NOT_MATCH';
+  value: string;
+
+  // Location data.
+  startOffset: number;
+  endOffset: number;
+  startLine: number;
+  endLine: number;
+  startColumn: number;
+  endColumn: number;
+}
+
+export interface SearchOperators {
+  /**
+   * Extract search terms and filters.
+   * @param search text to parse
+   * @param options parse options.
+   * @throws "Unexpected token Exception" exception, showing the actual
+   * line from the source, pointing with the ^ marker to the bad token.
+   */
+  parse(search: string, options?: ParserOptions): ParseResult;
+
+  /**
+   * Tokens corresponding to the last parse operation.
+   */
+  tokens: Token[];
+}
+
+declare const searchOperators: SearchOperators;
+
+export default searchOperators;
